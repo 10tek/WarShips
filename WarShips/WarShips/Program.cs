1c4c555b1fbc2b92a5a44c5aae959150fa2daa31
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-02-29 19:17:00 +0600

    Вывод карты (недоделан)

--- WarShips/WarShips/Program.cs
+++ WarShips/WarShips/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace WarShips
 {
@@ -6,7 +7,36 @@ namespace WarShips
     {
         static void Main(string[] args)
         {
+            ShowMap();
+            Console.ReadKey();
+        }
 
+        public static void ShowMap()
+        {
+            var letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            Console.SetCursorPosition(2, 0);
+            for(var i = 0; i < 10; i++)
+            {
+                Console.Write(" " + letters[i]);
+            }
+
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.SetCursorPosition(1 - (i)/10, i);
+                Console.Write(i);
+            }
+
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.SetCursorPosition(22, i);
+                Console.Write(i);
+            }
+
+            Console.SetCursorPosition(2, 11);
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write(" " + letters[i]);
+            }
         }
     }
 }
