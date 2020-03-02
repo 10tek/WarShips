25f693ffc3e75b3f04850915b0bd91acd67317fa
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-03-02 17:40:28 +0600

    Мелкие правки

--- WarShips/WarShips/Program.cs
+++ WarShips/WarShips/Program.cs
@@ -7,7 +7,7 @@ namespace WarShips
     {
         static void Main(string[] args)
         {
-            ShowMap();
+            Show();
             Console.ReadKey();
         }
 
@@ -15,6 +15,12 @@ namespace WarShips
         {
             var letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
             Console.SetCursorPosition(2, 0);
+            for(var i = 0; i < 10; i++)
+            {
+                Console.Write(" " + letters[i]);
+            }
+            
+            Console.SetCursorPosition(25, 0);
             for(var i = 0; i < 10; i++)
             {
                 Console.Write(" " + letters[i]);
@@ -36,6 +42,22 @@ namespace WarShips
             for (var i = 0; i < 10; i++)
             {
                 Console.Write(" " + letters[i]);
+            }
+        }
+
+        public static void Show()
+        {
+            var letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            for(var i = 0; i < 10; i++)
+            {
+                Console.SetCursorPosition(i*2+3, 0);
+                Console.Write(letters[i]);
+                Console.SetCursorPosition(i * 2 + 27, 0);
+                Console.Write(letters[i]);
+                Console.SetCursorPosition(1, i+1);
+                Console.Write(i);
+                Console.SetCursorPosition(22, i+1);
+                Console.Write(i);
             }
         }
     }
