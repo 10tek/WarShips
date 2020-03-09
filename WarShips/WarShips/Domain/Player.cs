c81d03d84dc3964b64c0130c89e37a2c806e41d9
NickSagalbaev <NickSagalbaev@gmail.com>
2020-03-09 13:43:26 +0600

    feat <Player> : Добавил класс player.

---
+++ WarShips/WarShips/Domain/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarShips.Interfaces;
+
+namespace WarShips.Domain
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<IShip> Ships { get; set; }
+        public int[,] Field { get; set; }
+        public int[,] EnemyField { get; set; }
+    }
+}
